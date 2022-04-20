aa3da3af299d6a4201f901c6dad77dc539c9a6ae
b1est <bondarenko2001@gmail.com>
2022-04-20 19:41:22 +0300

    final lab4 1.0

--- lab4/server.py
+++ lab4/server.py
@@ -1,10 +1,13 @@
 from http.server import HTTPServer, CGIHTTPRequestHandler
+from sys import platform
 import os
 
 
 # os.system('chmod a+x /cgi-bin/test_json.sh')
 
+if platform == "linux" or platform == "linux2":
+    os.system('sudo chmod a+x cgi-bin/request.py')
 server_address = ("", 8000)
 http_server = HTTPServer(server_address, CGIHTTPRequestHandler)
+
 http_server.serve_forever()
-os.system('sudo chmod a+x /cgi-bin/*.py')
